94a519ff502e7eeff2c403d98df8cbffb59972d0
Guillermo Alfaro <galfarotolon@gmail.com>
2020-09-17 17:57:28 -0500

    made a housing parser for the data that transforms into objects to add in Knex tables /seeds

--- models/cities-model.js
+++ models/cities-model.js
@@ -4,8 +4,9 @@ module.exports = {
   find,
   findByUser,
   findById,
-  findAllSummer,
-  findAllWinter,
+  // findAllSummer,
+  // findAllWinter,
+  findAllWinterAndSummer,
 };
 
 // get all cities 
@@ -21,19 +22,45 @@ function findByUser(user_id) {
     .where({ user_id })
 }
 
-// GET all data for summer
-function findAllSummer() {
-  return db('location')
-    .join('info_summer as is', 'is.location_id', 'location.id')
-    .select('location.location', 'is.*')
+// // GET all data for summer
+// function findAllSummer() {
+//   return db('location')
+//     .join('info_summer as is', 'is.location_id', 'location.id')
+//     .select('location.location', 'is.*')
+// }
+
+// // GET all data for winter
+// function findAllWinter() {
+//   return db('location')
+//     .join('info_winter as iw', 'iw.location_id', 'location.id')
+//     .select('location.location', 'iw.*')
+// }
+
+function findAllWinterAndSummer() {
+  return db('info_winter as w')
+    .join('info_summer as s')
+    .select('*')
+    .where('s.id, w.location_id')
 }
 
-// GET all data for winter
-function findAllWinter() {
-  return db('location')
-    .join('info_winter as iw', 'iw.location_id', 'location.id')
-    .select('location.location', 'iw.*')
-}
+
+
+
+// SELECT *
+// FROM info_summer as s 
+// INNER JOIN info_winter as w
+//       ON s.id= w.location_id
+// INNER JOIN location as l
+//       ON s.id=l.id
+
+
+// SELECT *
+// FROM info_summer as s
+// JOIN info_winter as w
+// ON s.location_id = w.id
+
+
+
 
 // GET city by id for WINTER table
 function findById(id) {
