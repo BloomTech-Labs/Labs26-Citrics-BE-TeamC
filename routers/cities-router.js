15a14d028b2f8468cbdc1983b0bcb72cf8a6aa16
Nhan <natenguyen51294@gmail.com>
2020-10-15 13:13:22 -0500

    added images for all cities in db

--- routers/cities-router.js
+++ routers/cities-router.js
@@ -74,36 +74,6 @@ router.get('/:id', async (req, res) => {
   }
 })
 
-// router.get('/:id', (req, res) => {
-//     const { id } = req.params;
-
-//     Cities.findById(id)
-//         .then(city => {
-//             res.status(200).json(city);
-//         })
-//         .catch(err => {
-//             res.status(500).json({ message: err })
-//         })
-// });
-
-
-//find cities saved from a specific user
-
-// router.get('/users/:id', (req, res) => {
-//     const { id } = req.params;
-//     Cities.findByUser(id)
-//         .then(city => {
-//             if (city) {
-//                 res.status(200).json(city);
-//             } else {
-//                 res.status(404).json({ message: 'there are no cities saved for this user' })
-//             }
-//         })
-//         .catch(err => {
-//             res.status(500).json({ message: err })
-//         })
-// });
-
 module.exports = router;
 
 
